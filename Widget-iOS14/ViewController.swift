8cd91f44f85967b3905b19cf2fcdc3fd60a4acf5
nsdevelopteam <63967267+nsdevelopteam@users.noreply.github.com>
2020-10-07 07:22:08 -0400

    Inital commit

---
+++ Widget-iOS14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Widget-iOS14
+//
+//  Created by Sina Rabiei on 10/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
